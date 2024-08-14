3d94b025f7290e8cd4459660a973cba4bdc08597
Samuel Boczek <samuelboczek@gmail.com>
2024-08-14 08:12:15 +0100

    fixes: don't regenerate .env if disabled from env.

--- src/rootfs/etc/notify.d/10-php.sh
+++ src/rootfs/etc/notify.d/10-php.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-if [[ "${LINE[0]}" != "/.env" ]] && [[ "${LINE[0]}" == *".env" ]]; then
-    # Expand changed .env files
-    echo "[notify.d/10-php.sh] $(date '+%Y-%m-%d %H:%M:%S') A \"${LINE[1]}\" to \"${LINE[0]}\" was detected, regenerating file ... "
-    expand_from_env "${LINE[0]}"
+source /usr/local/lib/shell/boolean.sh
+source /usr/local/lib/shell/expand-from-env.sh
 
+if is_true "$PHP_CONFIGURE_FROM_ENV"; then
+    if [[ "${LINE[0]}" != "/.env" ]] && [[ "${LINE[0]}" == *".env" ]]; then
+        # Expand changed .env files
+        echo "[notify.d/10-php.sh] $(date '+%Y-%m-%d %H:%M:%S') A \"${LINE[1]}\" to \"${LINE[0]}\" was detected, regenerating file ... "
+        expand_from_env "${LINE[0]}"
+    fi
+fi
+
+if [[ "${LINE[0]}" != "/.env" ]] && [[ "${LINE[0]}" == "/etc/php/$PHP_VERSION/"* ]]; then
     # fix file permissions
     chown -R "$USERNAME:$GROUPNAME" /etc/php/$PHP_VERSION
 
