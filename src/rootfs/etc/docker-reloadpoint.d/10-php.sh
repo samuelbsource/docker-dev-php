cef4eda3b2dd73916325e80d2da51d5eb70e21ac
Samuel Boczek <samuelboczek@gmail.com>
2024-08-14 08:16:40 +0100

    fixes: php does not reload on /.env change

--- src/rootfs/etc/docker-reloadpoint.d/10-php.sh
+++ src/rootfs/etc/docker-reloadpoint.d/10-php.sh
@@ -21,3 +21,9 @@ fi
 
 # fix file permissions
 chown -R "$USERNAME:$GROUPNAME" /etc/php/$PHP_VERSION
+
+# Reload php-fpm if conf changed
+if [ -f "/var/run/php-fpm.pid" ] && ps --pid $(cat /var/run/php-fpm.pid) > /dev/null; then
+    echo "[docker-reloadpoint.d/10-php.sh] $(date '+%Y-%m-%d %H:%M:%S') Reloading php-fpm ... "
+    $(which kill) --signal="USR2" $(cat /var/run/php-fpm.pid) # SIGUSR2 reloads conf and restarts all child workers
+fi
